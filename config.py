3dc1b5ddcc30df3ec931c8fa40747aea25cdc018
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-06-26 12:54:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN", "8821944749:AAERuFlZmsQ191GL8HOagSgtGYSkAMzaBL0")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7641508639").split()))
 SUDO_IDS = list(map(int, getenv("SUDO_IDS", "7641508639").split()))
-MONGO_DB = getenv("MONGO_DB", "sparsh")
+
+# 🔥 MONGODB ATLAS URL WITH DATABASE NAME
+MONGO_DB = "mongodb+srv://sparshshivare2606:sparshs2607@cluster0.cvditmt.mongodb.net/waifu_grabbers?retryWrites=true&w=majority"
+
 GEMINI_KEY = getenv("GEMINI_KEY", "AIzaSyAO4bufD0HIVd33aaB_YF36MHr4L7bmu04")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DevsHubChat")
