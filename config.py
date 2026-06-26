5f5e14ee29da79491092118c7a132c4af9367500
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-06-26 12:13:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "8821944749:AAERuFlZmsQ191GL8HOagSgtGYSkAMzaBL0")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7641508639").split()))
 SUDO_IDS = list(map(int, getenv("SUDO_IDS", "7641508639").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-GEMINI_KEY = getenv("GEMINI_KEY", "")
+MONGO_DB = getenv("MONGO_DB", "sparsh")
+GEMINI_KEY = getenv("GEMINI_KEY", "AIzaSyAO4bufD0HIVd33aaB_YF36MHr4L7bmu04")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DevsHubChat")
